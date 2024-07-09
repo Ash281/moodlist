f8657a4b83c8cb95a8c65677b259cd48a180d714
Ash281 <ashvin.valentine281@gmail.com>
2024-07-09 12:57:51 +0100

    install django

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ venv\Scripts\activate
 pip install -r requirements.txt
 npm install
 npm run build
+pip install django
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
