b20f950c737880d5f1eaec4f21f782e6cff6ad8c
Ash281 <ashvin.valentine281@gmail.com>
2024-07-09 12:37:14 +0100

    add venv to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
+python -m venv venv
+venv\Scripts\activate
 pip install -r requirements.txt
 npm install
 npm run build
+python manage.py makemigrations
+python manage.py migrate
 python manage.py collectstatic --noinput
