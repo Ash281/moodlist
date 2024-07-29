6e636eb179da070ec5aa7f20ea5934ec63e28704
Ash281 <ashvin.valentine281@gmail.com>
2024-07-29 23:21:35 +0100

    change requirements

--- build.sh
+++ build.sh
@@ -3,15 +3,6 @@ venv\Scripts\activate
 python3 -m pip install -r requirements.txt
 npm install
 npm run build
-pip install django
-pip install djangorestframework
-pip install django-cors-headers
-pip install whitenoise
-pip install torch
-pip install gunicorn
-pip install torchvision
-pip install facenet-pytorch
-pip install python-dotenv
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
